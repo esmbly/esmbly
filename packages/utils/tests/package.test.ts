f4c8e71ac1439265ddc23022f1c06441fe80f5eb
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-13 16:50:58 +0100

    test(utils): update test descriptions

--- packages/utils/tests/package.test.ts
+++ packages/utils/tests/package.test.ts
@@ -1,7 +1,7 @@
 import { getRoot, getRelativePathTo } from '../src/package';
 
 describe('getRoot', () => {
-  it('returns the root path', async () => {
+  it('resolves to the root path', async () => {
     const spy = jest.spyOn(process, 'cwd');
     spy.mockReturnValue('/some/path/');
     await expect(getRoot()).resolves.toEqual('/some/path/');
@@ -10,7 +10,7 @@ describe('getRoot', () => {
 });
 
 describe('getRelativePathTo', () => {
-  it('returns the relative path from the package root to the given file', async () => {
+  it('resolves to the relative path from the package root to the given file', async () => {
     const spy = jest.spyOn(process, 'cwd');
     spy.mockReturnValue('/some/path/');
     await expect(getRelativePathTo('/some/path/.esmblyrc.js')).resolves.toEqual(
