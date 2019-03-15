702ca7cdf6f15507115449561bc75c4087310a2f
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-16 00:57:12 +0100

    test(printer): add tests for printer

--- packages/printer/tests/index.test.ts
+++ packages/printer/tests/index.test.ts
@@ -1,3 +1,105 @@
-describe('output', () => {
-  it.todo('add tests');
+import { Writable } from 'stream';
+import printer from '../src';
+
+describe('printer', () => {
+  beforeEach(() => {
+    printer.setOutStream(process.stdout);
+    printer.setErrorStream(process.stderr);
+  });
+
+  describe('error', () => {
+    it('writes to process.stderr by default', () => {
+      const spy = jest.spyOn(process.stderr, 'write');
+      // @ts-ignore
+      spy.mockImplementation(() => {});
+      printer.error('error-message');
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy.mock.calls[0][0]).toEqual('error-message');
+      spy.mockRestore();
+    });
+  });
+
+  describe('print', () => {
+    it('writes to process.stdout by default', () => {
+      const spy = jest.spyOn(process.stdout, 'write');
+      // @ts-ignore
+      spy.mockImplementation(() => {});
+      printer.print('info-message');
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy.mock.calls[0][0]).toEqual('info-message');
+      spy.mockRestore();
+    });
+  });
+
+  describe('clearLine', () => {
+    it('clears the line for the provided stream(s)', () => {
+      const writeOut = jest.fn();
+      const writeErr = jest.fn();
+      const stdout = new Writable({ write: writeOut });
+      const stderr = new Writable({ write: writeErr });
+      (stdout as NodeJS.WriteStream).isTTY = true;
+      printer.setOutStream(stdout);
+      printer.setErrorStream(stderr);
+      printer.clearLine(printer.print, printer.error);
+      expect(writeOut).toHaveBeenCalledTimes(1);
+      expect(writeErr).toHaveBeenCalledTimes(1);
+      expect(writeOut.mock.calls[0][0].toString()).toEqual('\x1b[999D\x1b[K');
+      expect(writeErr.mock.calls[0][0].toString()).toEqual('\x1b[999D\x1b[K');
+    });
+  });
+
+  describe('isTTY', () => {
+    it('returns isTTY based on the current log stream', () => {
+      expect(printer.isTTY).toEqual(process.stdout.isTTY);
+      const customStream = new Writable();
+      printer.setOutStream(customStream);
+      expect(printer.isTTY).toEqual(false);
+    });
+  });
+
+  describe('setErrorStream', () => {
+    it('sets the error stream', () => {
+      let data = '';
+      const write = (chunk: string): void => {
+        data += chunk.toString();
+      };
+      const stream = new Writable({ write });
+      const spy = jest.spyOn(process.stderr, 'write');
+      printer.setErrorStream(stream);
+      printer.error('error-message');
+      expect(spy).not.toHaveBeenCalled();
+      expect(data).toEqual('error-message');
+    });
+
+    it('can be silenced by setting the stream to null', () => {
+      const spy = jest.spyOn(process.stderr, 'write');
+      printer.setErrorStream(process.stderr);
+      printer.setErrorStream(null);
+      printer.error('error-message');
+      expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('setLogStream', () => {
+    it('sets the log stream', () => {
+      let data = '';
+      const write = (chunk: string): void => {
+        data += chunk.toString();
+      };
+      const stream = new Writable({ write });
+      const spy = jest.spyOn(process.stdout, 'write');
+      printer.setOutStream(stream);
+      printer.print('info-message');
+      expect(spy).not.toHaveBeenCalled();
+      expect(data).toEqual('info-message');
+    });
+
+    it('can be silenced by setting the stream to null', () => {
+      const spy = jest.spyOn(process.stdout, 'write');
+      printer.setOutStream(process.stdout);
+      printer.setOutStream(null);
+      printer.print('info-message');
+      expect(spy).not.toHaveBeenCalled();
+    });
+  });
 });
