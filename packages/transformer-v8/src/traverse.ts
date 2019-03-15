fca28ce83e5d3064dd953645ab83d39afa177605
Olle Lauri Boström <ollebostr@gmail.com>
2019-03-15 11:20:28 +0100

    style(transformer-v8): fix lint issues

--- packages/transformer-v8/src/traverse.ts
+++ packages/transformer-v8/src/traverse.ts
@@ -1,6 +1,6 @@
-import * as rules from './rules';
 import { SyntaxTree } from '@esmbly/types';
 import traverse from '@babel/traverse';
+import * as rules from './rules';
 
 export default function(ast: SyntaxTree): void {
   traverse(ast.tree, {
