342027b526b432e7255176780ea1e0716c608774
Olle Lauri Boström <ollebostr@gmail.com>
2019-05-07 17:32:46 +0200

    fix(e2e): ignore deprecation warnings printed to stderr during e2e tests

--- e2e/helpers/testRunner.ts
+++ e2e/helpers/testRunner.ts
@@ -1,12 +1,21 @@
 import util from 'util';
 import { exec } from 'child_process';
 
+function isDeprecationWarning(stderr: string): boolean {
+  return (
+    stderr ===
+    '(node:7449) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.".length is not a function > "(node:7449) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.'
+  );
+}
+
 export async function testRunner(cwd: string): Promise<void> {
   const { stderr } = await util.promisify(exec)(`yarn run esmbly run`, { cwd });
-  console.log(`stderr: ${stderr}`);
-  console.log(`stderr.length: ${stderr.length}`);
 
-  if (stderr && stderr.trim().length > 0) {
+  if (
+    stderr &&
+    stderr.trim().length > 0 &&
+    !isDeprecationWarning(stderr.trim())
+  ) {
     throw new Error(stderr);
   }
 }
