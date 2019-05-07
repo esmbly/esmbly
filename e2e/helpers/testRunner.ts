321b1ca4f0d3dff4f48141c008d5a13c90106050
Olle Lauri Boström <ollebostr@gmail.com>
2019-05-07 17:56:45 +0200

    fix(e2e): ignore deprecation warnings during e2e tests

--- e2e/helpers/testRunner.ts
+++ e2e/helpers/testRunner.ts
@@ -3,27 +3,16 @@ import { exec } from 'child_process';
 
 function isDeprecationWarning(stderr: string): boolean {
   return (
-    stderr ===
-    '(node:7449) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.".length is not a function > "(node:7449) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.'
+    stderr.includes(
+      'DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.".length is not a function > "(node:7449) [DEP0005] DeprecationWarning: Buffer() is deprecated due to security and usability issues. Please use the Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() methods instead.',
+    ) && stderr.length === 190
   );
 }
 
 export async function testRunner(cwd: string): Promise<void> {
   const { stderr } = await util.promisify(exec)(`yarn run esmbly run`, { cwd });
-  console.log(
-    `IS DEPRECATION WARNING (trimmed): ${isDeprecationWarning(stderr.trim())}`,
-  );
-  console.log(
-    `IS DEPRECATION WARNING (not trimmed): ${isDeprecationWarning(stderr)}`,
-  );
-  console.log(`STDERR: ${stderr}`);
-  console.log(`STDERR.length: ${stderr.length}`);
 
-  if (
-    stderr &&
-    stderr.trim().length > 0 &&
-    !isDeprecationWarning(stderr.trim())
-  ) {
+  if (stderr && stderr.trim().length > 0 && !isDeprecationWarning(stderr)) {
     throw new Error(stderr);
   }
 }
